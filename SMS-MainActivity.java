f38448fe6e85e07d8edacde5caf3d8277c312b79
Jayeskumar <52567903+Jayeskumar@users.noreply.github.com>
2023-09-07 22:28:42 +0530

    Update SMS-MainActivity.java

--- SMS-MainActivity.java
+++ SMS-MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.sms;
+package com.example.cameraapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.telephony.gsm.SmsManager;
@@ -6,29 +6,38 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
+
 public class MainActivity extends Activity
 {
-@Override
-protected void onCreate(Bundle savedInstanceState
-{
-super.onCreate(savedInstanceState);
-setContentView(R.layout.activity_main);
-Button bt=(Button)findViewById(R.id.button1);
-bt.setOnClickListener(new OnClickListener()
-{
-@Override
-public void onClick(View v)
-{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button bt=(Button)findViewById(R.id.button1);
+        bt.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
 // TODO Auto-generated method stub
-SmsManager sms=SmsManager.getDefault();
-sms.sendTextMessage("5554", null, "hai", null, null);
-}
-});
-}
-public boolean onCreateOptionsMenu(Menu menu)
-{
-// Inflate the menu; this adds items to the action bar if it is present.
-getMenuInflater().inflate(R.menu.main, menu);
-return true;
-}
+                //Toast.makeText(getApplicationContext(),"Sucessfully Sent",Toast.LENGTH_LONG).show();
+                SmsManager sms=SmsManager.getDefault();
+
+                sms.sendTextMessage("5554", null, "hai", null, null);
+                Toast.makeText(getApplicationContext(),"Sucessfully Sent",Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+    /*
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+// Inflate the menu; this adds items
+// to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        Toast.makeText(getApplicationContext(),"Sucessfully Sent",Toast.LENGTH_SHORT).show();
+        return true;
+    }*/
 }
