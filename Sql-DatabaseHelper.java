62f52b9561a3dcda73304ce1afa64ece8aaf0df3
Jayeskumar <52567903+Jayeskumar@users.noreply.github.com>
2023-09-09 09:51:39 +0530

    Create Sql-DatabaseHelper.java

---
+++ Sql-DatabaseHelper.java
@@ -0,0 +1,37 @@
+package com.example.sqlapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "my_database";
+    public static final int DATABASE_VERSION = 1;
+    public static final String TABLE_NAME = "my_table";
+    //id for primary key
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_AGE = "age";
+
+    public static final String TABLE_CREATE =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_NAME + " TEXT, " +
+                    COLUMN_AGE + " INTEGER);";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
+
